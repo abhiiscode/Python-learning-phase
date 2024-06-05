cb44b94365946c01dbae63dfa9705e73772ba639
git_copilet <abktpass@gmail.com>
2024-06-05 14:35:57 +0530

    new version but add install PySimpleGUI package install

---
+++ day_16/FreeSimpleGUI.py
@@ -0,0 +1,28 @@
+# import day12_def_files
+import day16_def_source_file
+import PySimpleGUI as sg
+
+lable = sg.Text("Type in todo to-do")
+input_box= sg.InputText(tooltip="enter todo", key="todo")
+add_button = sg.Button("add")
+
+window = sg.Window('MY to-do app',
+                   layout=[[lable],
+                   [input_box, add_button]],
+                   font=('helvetica', 18))           # window is : object insest, type
+''' in every list inside a list and that list is 1 One row [[ ],[],[]] '''
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "add":
+            todo = day16_def_source_file.get_todos()
+            new_todo = values["todo"]
+            todo.append(new_todo)
+            day16_def_source_file.write_todos(todo)
+        case sg.WIN_CLOSED():
+            break
+window.close()
+
+
---
+++ day_16/day16_def_source_file.py
@@ -0,0 +1,10 @@
+FILEPATH = '../rows.txt'
+
+def get_todos(filepath=FILEPATH):  # it's only work when we call function (get_todos()) in new variable like : todos = get_todos()
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos(todos_arg, filepath=FILEPATH):  #filepath, todos_arg is the local variable/Parameters
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
